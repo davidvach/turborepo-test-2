372da5c4cc414bd9909eb2285b304325cf648729
davidvach <vachalek.david@gmail.com>
2024-12-25 12:42:56 +0100

    refactor(heroes): clean up formatting and remove redundant runtime declaration

--- apps/wiki/src/app/heroes/[heroId]/page.js
+++ apps/wiki/src/app/heroes/[heroId]/page.js
@@ -1,16 +1,13 @@
 import { allHeroes } from "../data";
 
-export const runtime = "static";
-
 export async function generateStaticParams() {
-    return allHeroes.map((hero) => ({
-        heroId: hero.id,
-    }));
+  return allHeroes.map((hero) => ({
+    heroId: hero.id,
+  }));
 }
 
 import HeroPage from "./HeroPage";
 
 export default function Page({ params }) {
-    return <HeroPage params={params} />;
+  return <HeroPage params={params} />;
 }
-  
